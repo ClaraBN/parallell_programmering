de42961fabade4028301a3cc4fd78b778daa7197
Clara Bernedal Nordstroem <clara.nordstrom@live.se>
2020-09-17 16:21:18 +0200

    first run findPrime

--- assignment2/sieve_of_ero.cpp
+++ assignment2/sieve_of_ero.cpp
@@ -8,8 +8,17 @@
 
 using namespace std;
 
-void findPrime(int *arraySeed, int *array){
-
+void findPrime(int *array, int endSeed, int min, int max){
+    int unmarked=0;
+    for(int seedEl=2; seedEl <= endSeed; seedEl++){
+        if(seedEl>0){
+            for(int chunkEl = min; chunkEl <= max; chunkEl++){
+                if(chunkEl%seedEl==0 && chunkEl>0){
+                    array[chunkEl-1]=-chunkEl;
+                };
+            };
+        };
+    };
 };
 
 int main(int argc, char *argv[]) {
@@ -28,8 +37,8 @@ int main(int argc, char *argv[]) {
   int max = 64;
  
   int sqrtMax = int(sqrt(max));
-  int seed[sqrtMax];
-  for(int i = 0; i < sqrtMax; i++){
+  volatile int seed[max];
+  for(int i = 0; i < max; i++){
       seed[i] = i+1;
       // printf("Seed: %d\n", seed[i]);
   };
@@ -41,7 +50,7 @@ int main(int argc, char *argv[]) {
       for(int j=pow(unmarked,2); j <= sqrtMax; j++){
           if(j%unmarked == 0){
               seed[j-1]=0-j;
-              printf("%s\n", "hej!");
+              // printf("%s\n", "hej!");
           };
       };
       bool nextUpdated = 0;
@@ -50,19 +59,20 @@ int main(int argc, char *argv[]) {
               nextUnmarked=seed[index];
               unmarked=nextUnmarked;
               nextUpdated = 1;
-              printf("Index: %d\n", index);
+              // printf("Index: %d\n", index);
               break;
           };
       };
       if(!nextUpdated){
-          printf("%s\n", "då!");
+          // printf("%s\n", "då!");
           break;
       };
   };
-  // just to check the code
-    for(int i = 0; i < sqrtMax; i++){
-      printf("Seed%d: %d\n", i, seed[i]);
-  };
-
+    // int numThreads = stoi(argv[2]);
+    findPrime(*seed, sqrtMax, sqrtMax+1, max);
+    // just to check the code
+    for(int i = 0; i < max; i++){
+       printf("Seed%d: %d\n", i, seed[i]);
+       };
   return 0;
 };
