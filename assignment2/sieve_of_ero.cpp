baeb6ee529f7a544e13e18d7caf4610c6e18e25c
Clara Bernedal Nordstroem <clara.nordstrom@live.se>
2020-09-17 14:41:18 +0200

    testing to print seed with sqrtMax

---
+++ assignment2/sieve_of_ero.cpp
@@ -0,0 +1,36 @@
+# include <thread>
+# include <iostream>
+# include <stdio.h>
+# include <math.h>
+# include <future>
+# include <string.h>
+# include <chrono>
+
+using namespace std;
+
+void findPrime(int *array){
+
+};
+
+int main(int argc, char *argv[]) {
+    
+  if (argc != 2) {
+    char h[] = {'-','h'};
+    if (strcmp(argv[1], h) == 0) {
+      printf("%s\n","First argument is the number of threads (integer), the second argument is number of trapezes(integer) and trapezes should be a multiple of threads");
+      return 0;
+    };
+    printf("%s\n", "invalid input. try -h for help");
+    return 0;
+  };
+
+  // int max = stoi(argv[1]);
+  int max = 25;
+ 
+  int sqrtMax = int(sqrt(max));
+  int seed[sqrtMax];
+  for(int i = 0; i < sqrtMax; i++){
+      seed[i] = i;
+  };
+  return 0;
+};
