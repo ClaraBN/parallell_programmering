ce784e9095e0f5f69404808663dc14b3e4d6e22a
Clara Bernedal Nordstroem <clara.nordstrom@live.se>
2020-09-24 20:55:46 +0200

    testing rand()

--- assignment2/integral.cpp
+++ assignment2/integral.cpp
@@ -5,6 +5,7 @@
 # include <future>
 # include <string.h>
 # include <chrono>
+# include <random>
 
 using namespace std;
 
@@ -44,46 +45,79 @@ int main(int argc, char *argv[]) {
     return 0;
   };
 
+  // saving the inputs as variables 
   int numThreads = stoi(argv[1]);
   int trapz = stoi(argv[2]);
 
+  // making all the threads
+  future<double> futures[numThreads];
+
+
+        /*        ************************         */
+        /*           EQUAL DISTRIBUTION            */
+        /*        ************************         */
+
+
+  // Calculate how to distrubute trapz per threads
   double trapzPerThread = trapz/numThreads;
   double intervalStep = 1/double(numThreads);
   double interval[int(trapzPerThread)];
   interval[0] = 0;
   
+  // saving the interval into an array
   for (int i = 1; i <= numThreads; i++) {
     interval[i] = interval[i-1] + intervalStep;
   };
 
-  //thread *threads = new thread[numThreads];
-
-  future<double> futures[numThreads];
+  // starting timer
   auto start_time = chrono::system_clock::now();
+
+  // for each thread run "compute"
   for (int j = 0; j < numThreads; j++){
     futures[j] = async(compute, interval[j], interval[j+1], trapzPerThread);
   };
 
+  // sum all the threads results into a result 
   double result = 0;
   for (int j = 0; j < numThreads; j++){
     double n = futures[j].get();
     result = result + n;
   };
 
-
-    //for (int threadID = 0; threadID < numThreads; threadID++){
-    //threads[threadID] = thread(compute, interval[threadID], interval[threadID+1], trapzPerThread);
-    //};
-
-  //double result = threads[0].get();
-
-  //calculating the time
+  //calculating the runtime and write out tut to the terminal
   chrono::duration<double> duration = (chrono::system_clock::now() - start_time);
-  cout << "Result: " << result << "\n" << "Duration: " << duration.count() << "\n";
+  cout << "even distribution: \n" << "Result: " << result << "\n" << "Duration: " << duration.count() << endl;
 
+
+  /*   *************************   */
+  /*      RANDOM DISTRIBUTION      */
+  /*   *************************   */
+
+
+  // Calculate how to distrubute trapz per threads
+      
+
+  // saving the interval into an array
+  for (int i = 1; i <= numThreads; i++) {
+    double random = rand() % 1 + 0.01; // in the range 0.01 to 1
+    cout << random << endl
+  };
+  // starting timer
+  //auto start_time = chrono::system_clock::now();
+
+  // for each thread run "compute"
+
+  // sum all the threads results into a result 
+
+  //calculating the runtime and write out tut to the terminal
+
+
+
+
+
+  //thread *threads = new thread[numThreads];
   //threads[0].join();
-
   //delete[] threads;
   return 0;
 
-    };
+  };
